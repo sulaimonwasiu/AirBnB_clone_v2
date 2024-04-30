db86ab1fdce5c4f2a14500e89ba7c0459d5b392f
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-04-30 08:18:23 +0300

    Basic db test added

---
+++ tests/test_models/test_engine/test_mysql_db.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Module for testing db storage"""
+
+
+import os
+import unittest
+import MySQLdb
+
+
+class MyDatabaseTestCase(unittest.TestCase):
+    """MySQL database tester"""
+
+    def setUp(self):
+        # Retrieve environment variables or use default values
+        host = os.environ.get('HBNB_MYSQL_HOST', 'localhost')
+        user = os.environ.get('HBNB_MYSQL_USER', 'test_user')
+        password = os.environ.get('HBNB_MYSQL_PWD', 'test_password')
+        database = os.environ.get('HBNB_MYSQL_DB', 'test_database')
+
+        # Connect to the testing database
+        self.connection = MySQLdb.connect(
+            host=host,
+            user=user,
+            passwd=password,
+            db=database
+        )
+        self.cursor = self.connection.cursor()
+
+    def tearDown(self):
+        # Close the database connection
+        self.connection.close()
+
+    def test_something(self):
+        # Perform a test using the database connection
+        #query = "SELECT * FROM my_table"
+        #self.cursor.execute(query)
+        #result = self.cursor.fetchall()
+        #self.assertEqual(len(result), 2)
+        pass
