9f5021abe67ba135dc34a5f8fa5fac4ff735b0ef
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-05-02 05:17:08 +0300

    .

--- models/city.py
+++ models/city.py
@@ -10,8 +10,9 @@ from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """Representation of city """
+    __tablename__ = 'cities'
     if models.storage_type == "db":
-        __tablename__ = 'cities'
+        
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
     else:
