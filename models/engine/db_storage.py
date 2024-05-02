bab708a4c0b670da041b84d2425da5b3972feb89
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-05-02 05:02:26 +0300

    .

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """DBStorage module for the HBNB project"""
 import os
+import models
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.base_model import Base
 
 
