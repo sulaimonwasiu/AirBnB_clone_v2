1b348df8024c05f690ea681533d798cef3cc43f1
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-05-02 03:09:25 +0300

    Error fix

--- models/state.py
+++ models/state.py
@@ -1,22 +1,29 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+""" holds class State"""
+import models
 from models.base_model import BaseModel, Base
 from models.city import City
-import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class State(BaseModel, Base):
-    """ The state class, contains state name """
-    from models.__init__ import storage_type
-    if storage_type == "db":
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="state")
+    """Representation of state """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    if models.storage_type == "db":
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
         @property
         def cities(self):
-            """Getter attribute that returns the list of City instances
-            with state_id equals to the current State.id"""
-            return [city for city in models.storage.all(City).values()
-                    if city.state_id == self.id]
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
