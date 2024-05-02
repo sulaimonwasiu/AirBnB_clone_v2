6576e8cab14f8e719161446468835c750a5eecfd
Wasiu Sulaimon <sulaimonwasiu13@gmail.com>
2024-05-02 06:05:26 +0300

    .

--- models/user.py
+++ models/user.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """Representation of a user """
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    if models.storage_t == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
